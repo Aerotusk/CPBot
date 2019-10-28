4641f55d5fcb13313eb1cc65cdae1421096b902e
Aerotusk <odannyboy001@gmail.com>
2019-10-28 13:14:25 -0700

    Added names, program, and description

--- Week_5/A_Counted_Loop/A_Counted_Loop.ino
+++ Week_5/A_Counted_Loop/A_Counted_Loop.ino
@@ -1,3 +1,10 @@
+/* 
+   Austin Gnecco, Danny Maas, Jessica Pearson, Saket Shanmugam
+   Program: For Four Squared Loop-the-Loop
+   Goal: Use a for loop in order to trace a square, then pause after
+   each square trance and flash the LED for three seconds.
+*/
+
 int motorA1 = 4; // IN1 (Motor A)
 int motorA2 = 5; // IN2 (Motor A)
 int motorB1 = 7; // IN3 (Motor B)
