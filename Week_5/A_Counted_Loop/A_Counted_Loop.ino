570e7b3e8a1506983a6c2fd8a040e9be03c30b74
Aerotusk <odannyboy001@gmail.com>
2019-10-28 13:26:21 -0700

    Standardized code
    Variable names, added functions

--- Week_5/A_Counted_Loop/A_Counted_Loop.ino
+++ Week_5/A_Counted_Loop/A_Counted_Loop.ino
@@ -5,54 +5,72 @@
    each square trance and flash the LED for three seconds.
 */
 
-int motorA1 = 4; // IN1 (Motor A)
-int motorA2 = 5; // IN2 (Motor A)
-int motorB1 = 7; // IN3 (Motor B)
-int motorB2 = 6; // IN4 (Motor B)
+int right1 = 4; // IN1 (Motor A)
+int right2 = 5; // IN2 (Motor A)
+int left1 = 7; // IN3 (Motor B)
+int left2 = 6; // IN4 (Motor B)
 int LED = 13; // LED pin
+int full_speed = 255;
+int forward_time = 1000;
+int turn_time = 255;
 
 void setup()
 {
- pinMode(motorA1, OUTPUT);
- pinMode(motorA2, OUTPUT);
- pinMode(motorB1, OUTPUT);
- pinMode(motorB2, OUTPUT);
+ pinMode(right1, OUTPUT);
+ pinMode(right2, OUTPUT);
+ pinMode(left1, OUTPUT);
+ pinMode(left2, OUTPUT);
  pinMode(LED,OUTPUT);
-
- //Repeat this four times:
- for(int i = 0;i<4;i++)
- {
-  //Move Forwards
-  digitalWrite(motorA1, HIGH);
-  digitalWrite(motorA2, LOW);
-  digitalWrite(motorB1, HIGH);
-  digitalWrite(motorB2, LOW);
-  delay(1000);
-
-  //Stop motors temporarily before turning
-  digitalWrite(motorA1, LOW);
-  digitalWrite(motorB1, LOW);
-  delay(100);
-  
-  //Turn
-  digitalWrite(motorA1, HIGH);
-  digitalWrite(motorB2, HIGH);
-  delay(250);
-
-  //Stop Motors before continuing
-  digitalWrite(motorA1, LOW);
-  digitalWrite(motorB2, LOW);
-  delay(100);
-  }
-  
-  //Flash LED, pause for 3 seconds
-  digitalWrite(LED,HIGH);
-  delay(1000);
-  digitalWrite(LED,LOW);
-  delay(2000);
 }
 
 void loop()
 {
- 
+ //Repeat this four times:
+ for(int i = 0;i<4;i++)
+ {
+  Trace1Side();
+  PivotRight();
+ }
+ flashLED(1000);
+ delay(2000);
+}
+
+//Forward
+void Trace1Side()
+{
+  analogWrite(right1, full_speed);
+  digitalWrite(right2, LOW);
+  analogWrite(left1, full_speed);
+  digitalWrite(left2, LOW);
+  delay(forward_time);
+  //Stop Motors
+  digitalWrite(right1, LOW);
+  digitalWrite(right2, LOW);
+  digitalWrite(left1, LOW);
+  digitalWrite(left2, LOW);
+  delay(100);
+}
+
+// Turn 90 degrees
+void PivotRight()
+{
+  analogWrite(right2, full_speed);
+  digitalWrite(right1, LOW);
+  analogWrite(left1, full_speed);
+  digitalWrite(left2, LOW);
+  delay(turn_time);
+  //Stop Motors
+  digitalWrite(right1, LOW);
+  digitalWrite(right2, LOW);
+  digitalWrite(left1, LOW);
+  digitalWrite(left2, LOW);
+  delay(100);
+}
+
+//Flash
+void flashLED(int flash_interval)
+{
+  digitalWrite(LED, HIGH);
+  delay(flash_interval);
+  digitalWrite(LED, LOW);
 }
