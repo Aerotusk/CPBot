2a221eefb8080d9e4d9e42682f21291a4c0df03f
Aerotusk <odannyboy001@gmail.com>
2019-10-28 13:10:07 -0700

    Added code comments
    Also fixed an error or two.
    
    Mainly: code would pause three seconds after each side, not after the entire square, and after completing each movement, the robot needs to stop for a short time in order to ensure accuracy. (See lines 25-27 and 35-37).

--- Week_5/A_Counted_Loop/A_Counted_Loop.ino
+++ Week_5/A_Counted_Loop/A_Counted_Loop.ino
@@ -1,7 +1,7 @@
 int motorA1 = 4; // IN1 (Motor A)
 int motorA2 = 5; // IN2 (Motor A)
-int motorB1 = 6; // IN3 (Motor B)
-int motorB2 = 7; // IN4 (Motor B)
+int motorB1 = 7; // IN3 (Motor B)
+int motorB2 = 6; // IN4 (Motor B)
 int LED = 13; // LED pin
 
 void setup()
@@ -12,30 +12,37 @@ void setup()
  pinMode(motorB2, OUTPUT);
  pinMode(LED,OUTPUT);
 
- for(int i = 1;i<5;i++){
+ for(int i = 0;i<4;i++)
+ {
+  //Move Forwards
   digitalWrite(motorA1, HIGH);
   digitalWrite(motorA2, LOW);
   digitalWrite(motorB1, HIGH);
   digitalWrite(motorB2, LOW);
-  delay(2000);
+  delay(1000);
 
+  //Stop motors temporarily before turning
+  digitalWrite(motorA1, LOW);
   digitalWrite(motorB1, LOW);
+  delay(100);
+  
+  //Turn
+  digitalWrite(motorA1, HIGH);
   digitalWrite(motorB2, HIGH);
   delay(250);
 
+  //Stop Motors before continuing
   digitalWrite(motorA1, LOW);
   digitalWrite(motorB2, LOW);
-  delay(3000);
-  
+  delay(100);
   }
   digitalWrite(LED,HIGH);
   delay(1000);
-
   digitalWrite(LED,LOW);
+  delay(2000);
 }
 
 void loop()
 {
  
- }
- 
+}
